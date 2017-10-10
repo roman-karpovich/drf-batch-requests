825f6c95c050e1a862bc42c1ca8b878cc3a1d8a4
Roman Karpovich <roman@razortheory.com>
2017-10-10 15:34:51 +0300

    updated setup configs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'readme.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -10,10 +10,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='drf-batch-requests',
-    version='0.0.1',
+    version='0.8.0',
     packages=['drf_batch', ],
     include_package_data=True,
-    license='BSD License',
+    license='MIT License',
     description='Facebook-like implementation of batch requests',
     long_description=README,
     keywords='django django-rest-framework drf batch',
@@ -22,21 +22,23 @@ setup(
     author_email='fpm.th13f@gmail.com',
     install_requires=[
         'django',
-        # 'drf-batch-requests',
+        'django-rest-framework',
     ],
     classifiers=[
-        # 'Development Status :: 3 - Alpha',
-        # 'Environment :: Web Environment',
-        # 'Framework :: Django',
-        # 'Intended Audience :: Developers',
-        # 'License :: OSI Approved :: BSD License',
-        # 'Operating System :: OS Independent',
-        # 'Programming Language :: Python',
-        # 'Programming Language :: Python :: 2',
-        # 'Programming Language :: Python :: 2.6',
-        # 'Programming Language :: Python :: 2.7',
-        # 'Topic :: Internet :: WWW/HTTP',
-        # 'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     zip_safe=True
 )
