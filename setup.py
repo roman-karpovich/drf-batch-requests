224d2a7b0e8222bb82cbc933c2f9adca5eabe6ef
Roman Karpovich <fpm.th13f@gmail.com>
2022-06-17 10:55:32 +0800

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='drf-batch-requests',
-    version='0.9.3',
+    version='0.9.4',
     packages=['drf_batch_requests', ],
     include_package_data=True,
     license='MIT License',
